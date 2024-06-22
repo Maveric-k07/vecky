6b6bb69ccd4aaa40b7189b8c4dc09ea3f3910505
Akhil <gurramakhileshwar333@gmail.com>
2024-06-22 13:57:24 +0530

    add multi embedding model support

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
-from sqlalchemy import create_engine, Column, select
-from sqlmodel import Field, SQLModel
-from sqlalchemy.orm import sessionmaker
-from pgvector.sqlalchemy import Vector
-from typing import List, Optional
-from sentence_transformers import SentenceTransformer
-import time
+from abc import ABC, abstractmethod
 from enum import Enum
+from typing import List, Union, Optional
+from sentence_transformers import SentenceTransformer
+from openai import OpenAI
+from sqlalchemy import Column, create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlmodel import Field, SQLModel, select
+from pgvector.sqlalchemy import Vector
+import os
 from uuid import UUID, uuid4
+import time
 
 
 def measure_time(function):
@@ -17,6 +20,50 @@ def measure_time(function):
         return result, execution_time
 
     return wrapper
+
+
+class BaseEmbeddingModel(ABC):
+    @abstractmethod
+    def encode(self, text: Union[str, List[str]]) -> List[float]:
+        pass
+
+    @property
+    @abstractmethod
+    def dimension(self) -> int:
+        pass
+
+
+class SentenceTransformerModel(BaseEmbeddingModel):
+    def __init__(self, model_name: str):
+        self.model = SentenceTransformer(model_name)
+
+    def encode(self, text: Union[str, List[str]]) -> List[float]:
+        return self.model.encode(text).tolist()
+
+    @property
+    def dimension(self) -> int:
+        return self.model.get_sentence_embedding_dimension()
+
+
+class OpenAIEmbeddingModel(BaseEmbeddingModel):
+    def __init__(self, model_name: str = "text-embedding-3-small"):
+        self.client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+        self.model_name = model_name
+
+    def encode(self, text: Union[str, List[str]]) -> List[float]:
+        if isinstance(text, str):
+            text = [text]
+        response = self.client.embeddings.create(input=text, model=self.model_name)
+        return response.data[0].embedding
+
+    @property
+    def dimension(self) -> int:
+        return 1536  # OpenAI's embedding dimension
+
+
+class EmbeddingModelType(Enum):
+    SENTENCE_TRANSFORMER = "sentence_transformer"
+    OPENAI = "openai"
 
 
 class SimilarityMetric(Enum):
@@ -30,14 +77,27 @@ class Document(SQLModel, table=True):
     id: UUID = Field(default_factory=uuid4, primary_key=True, index=True)
     collection_name: str = Field(default="vecky", index=True)
     content: str
-    embedding: List[float] = Field(sa_column=Column(Vector(384)))
+    embedding: List[float] = Field(
+        sa_column=Column(Vector(1536))
+    )  # Use the largest dimension
+
+
+def ensure_embedding_dimension(embedding, target_dim=1536):
+    if len(embedding) > target_dim:
+        return embedding[:target_dim]
+    elif len(embedding) < target_dim:
+        return embedding + [0.0] * (target_dim - len(embedding))
+    return embedding
 
 
 class VectorDatabase:
     DEFAULT_COLLECTION = "vecky"
 
     def __init__(
-        self, db_url, embedding_model, similarity_metric=SimilarityMetric.COSINE
+        self,
+        db_url: str,
+        embedding_model: BaseEmbeddingModel,
+        similarity_metric: SimilarityMetric = SimilarityMetric.COSINE,
     ):
         self.engine = create_engine(db_url)
         SQLModel.metadata.create_all(self.engine)
@@ -57,24 +117,37 @@ class VectorDatabase:
     @classmethod
     def from_documents(
         cls,
-        db_url,
-        docs,
-        collection_name=None,
-        embedding_model=None,
-        similarity_metric=SimilarityMetric.COSINE,
+        db_url: str,
+        docs: List[str],
+        embedding_model_type: EmbeddingModelType,
+        model_name: str,
+        collection_name: Optional[str] = None,
+        similarity_metric: SimilarityMetric = SimilarityMetric.COSINE,
     ):
+        if embedding_model_type == EmbeddingModelType.SENTENCE_TRANSFORMER:
+            embedding_model = SentenceTransformerModel(model_name)
+        elif embedding_model_type == EmbeddingModelType.OPENAI:
+            embedding_model = OpenAIEmbeddingModel(model_name)
+        else:
+            raise ValueError(
+                f"Unsupported embedding model type: {embedding_model_type}"
+            )
+
         instance = cls(db_url, embedding_model, similarity_metric)
         instance.add_documents(docs, collection_name)
         return instance
 
-    def add_documents(self, docs, collection_name=None):
+    def add_documents(self, docs: List[str], collection_name: Optional[str] = None):
         collection_name = collection_name or self.DEFAULT_COLLECTION
         session = self.Session()
         try:
             for doc in docs:
-                embedding = self.embedding_model.encode(doc).tolist()
+                embedding = self.embedding_model.encode(doc)
+                embedding = ensure_embedding_dimension(embedding=embedding)
                 db_doc = Document(
-                    content=doc, embedding=embedding, collection_name=collection_name
+                    content=doc,
+                    embedding=embedding,
+                    collection_name=collection_name,
                 )
                 db_doc = Document.model_validate(db_doc)
                 session.add(db_doc)
@@ -82,19 +155,20 @@ class VectorDatabase:
         finally:
             session.close()
 
-    def set_similarity_metric(self, metric):
-        self.similarity_metric = metric
-        self._set_similarity_function()
-
     @measure_time
     def search(self, query: str, collection_name: Optional[str] = None, k: int = 5):
-        query_embedding = self.embedding_model.encode(query).tolist()
+        query_embedding = self.embedding_model.encode(query)
+        query_embedding = ensure_embedding_dimension(query_embedding)
+
         session = self.Session()
         try:
+            similarity_function = self._sim_func(
+                Document.embedding, query_embedding
+            ).label("similarity")
             stmt = select(
                 Document.content,
-                self._sim_func(Document.embedding, query_embedding).label("similarity"),
-            ).order_by(self._sim_func(Document.embedding, query_embedding))
+                similarity_function,
+            ).order_by(similarity_function)
 
             if collection_name:
                 stmt = stmt.filter(Document.collection_name == collection_name)
@@ -107,17 +181,14 @@ class VectorDatabase:
 
     def __repr__(self):
         session = self.Session()
-        try:
-            total_docs = session.query(Document).count()
-            collections = session.query(Document.collection_name.distinct()).count()
-            return (
-                f"VectorDatabase(total_documents={total_docs}, "
-                f"num_collections={collections}, "
-                f"embedding_model={self.embedding_model.__class__.__name__}, "
-                f"similarity_metric={self.similarity_metric.name})"
-            )
-        finally:
-            session.close()
+        total_docs = session.query(Document).count()
+        collections = session.query(Document.collection_name.distinct()).count()
+        return (
+            f"VectorDatabase(total_documents={total_docs}, "
+            f"num_collections={collections}, "
+            f"embedding_model={self.embedding_model.__class__.__name__}, "
+            f"similarity_metric={self.similarity_metric.name})"
+        )
 
     def list_collections(self):
         session = self.Session()
@@ -142,8 +213,9 @@ class VectorDatabase:
 
 if __name__ == "__main__":
     print("Loading embedding model...")
-    model = SentenceTransformer("sentence-transformers/all-MiniLM-L12-v2")
 
+    sentence_transformer_model = "sentence-transformers/all-MiniLM-L12-v2"
+    openai_embedding_model = "text-embedding-3-small"
     db_url = "postgresql://postgres:password@localhost:5433/vector_db"
 
     document = """She found the old lockbox buried under the floorboards, its metal surface tarnished with age. With trembling hands, she twisted the rusty latch and gasped at its contents - a stack of letters tied with faded ribbon, each bearing her grandmother's elegant script from decades past.
@@ -157,7 +229,7 @@ No one could quite remember when the old railway handcar had been abandoned besi
 Though Jim's body had failed him long ago, he blessed the twilight years that had sharpened his mind's eye. From his worn rocking chair, he constantly reimagined the dance of the clouds into fantastic beasts and sailing ships bound for lands unseen by weathered mariners.
 The ramshackle cottage sat placid amid the gradual encroachment of the marsh thickets. Lillian would never glimpse the rambling rose vines that had wound the crumbling chimney, but she could smell their haunting sweet perfume drifting through the skeins of mist swirling beneath the moon's watchful gaze."""
 
-    collection_name = "park-stories"
+    collection_name = "park-stories-3"
 
     docs = document.split("\n")
     print(f"Creating vector database for {len(docs)} documents...")
@@ -165,10 +237,12 @@ The ramshackle cottage sat placid amid the gradual encroachment of the marsh thi
     vdb = VectorDatabase.from_documents(
         db_url=db_url,
         docs=docs,
+        embedding_model_type=EmbeddingModelType.OPENAI,
+        model_name=openai_embedding_model,
         collection_name=collection_name,
-        embedding_model=model,
         similarity_metric=SimilarityMetric.COSINE,
     )
+
     print(vdb)
 
     query = "What did Emma do in this story?"
@@ -185,10 +259,6 @@ The ramshackle cottage sat placid amid the gradual encroachment of the marsh thi
     ]
     vdb.add_documents(new_docs, collection_name=collection_name)
     print(f"\nAdded {len(new_docs)} new documents. Total documents: {vdb}")
-
-    # Changing similarity metric
-    vdb.set_similarity_metric(SimilarityMetric.EUCLIDEAN)
-    print(f"Changed similarity metric to {vdb.similarity_metric.name}")
 
     (results, scores), search_time = vdb.search(query, k=3)
 
